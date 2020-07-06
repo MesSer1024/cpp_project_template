663246257a3d854606139205194f28f140269248
Daniel <messer_@hotmail.com>
2020-07-06 17:24:33 +0200

    fixing compile error in final

--- template_lib/_Public/template_lib/Types.h
+++ template_lib/_Public/template_lib/Types.h
@@ -23,6 +23,6 @@ namespace ddahlkvist
 #if defined(DD_DEBUG) || defined(DD_RELEASE)
 #define DD_ASSERT(x) assert(x)
 #elif defined (DD_FINAL)
-#define DD_ASSERT(x) do {} while {false}
+#define DD_ASSERT(x) do {} while (false)
 #endif
 }
